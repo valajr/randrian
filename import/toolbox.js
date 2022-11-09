a0953d341aff3f8b22238baf5ba987738f55c0ed
Hiusu <luis_vala_junior@hotmail.com>
2022-11-08 21:40:11 -0300

    documentation of toolbox

--- import/toolbox.js
+++ import/toolbox.js
@@ -1,3 +1,10 @@
+/**
+ * A function to get a number between minimum and maximum given values.
+ * 
+ * @param {number} min - minimum range of random
+ * @param {number} max - maximum range of random
+ * @returns a integer between minimum and maximum given values
+ */
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
